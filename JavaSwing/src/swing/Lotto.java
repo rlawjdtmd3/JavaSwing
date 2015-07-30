2e1010361bcc5d3c291576b5879d1392a167bf9d
DAILY <DAILY@ITBANK22>
2015-07-30 15:25:54 +0900

    2015 .07. 30    JavaSwing 시작

---
+++ JavaSwing/src/swing/Lotto.java
@@ -0,0 +1,9 @@
+package swing;
+/*
+ * @ Date : 2015.07.30
+ * @ Author : me
+ * @ Story : 
+ * */
+public class Lotto {
+
+}
---
+++ JavaSwing/src/swing/LottoUI.java
@@ -0,0 +1,24 @@
+package swing;
+/*
+ * @ Date : 2015.07.30
+ * @ Author : me
+ * @ Story : 
+ * */
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+// UI : User Interface 사용자 화면
+public class LottoUI extends JFrame implements ActionListener{
+
+	public LottoUI() {
+	} //생성자까지  ,, 여기다 버튼을꽂으면 모양으로 뜸    어떻게 ? 위에 가져다쓴 인터페이스 ,, 클래스가 알아서해줌 ....
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}//imple후
+
+}
